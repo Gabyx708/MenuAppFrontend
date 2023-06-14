a9effcde10764a93fabb17f7310917750953c581
Gabriel Coria <matiascoria427@gmail.com>
2023-06-14 10:42:36 -0300

    se agrego la respuesta del menu , se estiliso el sidebar y la respuesta del menu

--- AdminFront/js/components/cardMenu.js
+++ AdminFront/js/components/cardMenu.js
@@ -5,30 +5,24 @@ export default function cardMenu(menuData){
     let platillosMenu = menuData.platillos;
     let platoData = [];
     console.log(platillosMenu);
-    console.log("original --------------------------------------");
 
-    platillosMenu.forEach( async (plato) => {
-        let id = plato.id;
-        let descripcion = await conseguirDescripcion(id);
-        plato.descripcion = descripcion;
-        platoData.push(plato);
-    });
 
     
 
        console.log(platoData);
 
-       let platillosImprimir = platoData.map(p => `<tr><th scope="row">${p.id}</th><td>$${p.descripcion}</td><td>${p.stock}</td><td>$${p.precio}</td></tr>`).join('');
+       let platillosImprimir = platillosMenu.map(p => `<tr><th scope="row">${p.id}</th><td>${p.descripcion}</td><td>${p.stock}</td><td>$${p.precio}</td></tr>`).join('');
     
     return (
-        '<div class="card tarjetaComanda">' +
+        '<div class="card ">' +
+        '<h2>Menu</h2><hr>'+
         '<div class="card-body">' +
-        '<span class="label-comanda">codigo: '+menuData.id+'</span>'+
-          '<span class="label-comanda">fecha: '+menuData.fecha_consumo+'</span>'+
-          '<span class="label-comanda">entrega: '+menuData.fecha_carga+'</span>'+
-          '<span class="label-comanda">entrega: '+menuData.fecha_cierre+'</span>'+
+        '<div class="info-container"><span class="label-menu bg-warning">codigo: '+menuData.id+'</span>'+
+          '<span class="label-menu  bg-warning">fecha consumo: '+menuData.fecha_consumo+'</span>'+
+          '<span class="label-menu  bg-warning">fecha de carga: '+menuData.fecha_carga+'</span>'+
+          '<span class="label-menu  bg-warning">fecha de cierre: '+menuData.fecha_cierre+'</span> </div>'+
           '<table class="table table-bordered">'+
-          '<thead><tr><th scope="col">codigo interno</th><th scope="col">stock</th><th scope="col">precio</th></tr></thead>'+
+          '<thead><tr><th scope="col">codigo interno</th><th scope="col">descripcion</th><th scope="col">stock</th><th scope="col">precio</th></tr></thead>'+
           '<tbody class="table-group-divider">'+platillosImprimir+
           '</tbody></table>'+
         '</div>' +
