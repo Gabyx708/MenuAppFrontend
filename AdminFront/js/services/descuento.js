4a0447b0c3b290ba8abddfa29458fbe0176e97c6
Gabriel <matiascoria427@gmail.com>
2023-06-26 08:18:34 -0300

    se empieza a definir la logica para el descuento

---
+++ AdminFront/js/services/descuento.js
@@ -0,0 +1,52 @@
+import config from "../../config/config.js";
+
+const api = config.apiUrl;
+const enpointDescuento = `${api}/api/Descuento`;
+
+const crearDescuento = async (descuentoRequest) => {
+
+    let result;
+    const response = await fetch(enpointDescuento, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(menuRequest),
+      });
+  
+      if (!response.ok) {
+        throw new Error();
+      }
+
+      if(response.ok){
+        result = await response.json();
+      }
+      
+      return result; 
+
+};
+
+const conseguirDescuentoVigente = async () => {
+
+    let result;
+
+    const response = await fetch(enpointPlatillo);
+    
+      if (!response.ok) {
+        throw new Error();
+      }
+
+      if(response.ok){
+        result = await response.json();
+      }
+  
+      return result;
+
+};
+
+
+export const Descuento = {
+
+    Post : crearDescuento ,
+    Get : conseguirDescuentoVigente
+};
