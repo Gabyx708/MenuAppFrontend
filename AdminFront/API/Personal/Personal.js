ea9e7f05e6038af086e6d29e496cdf78619bb707
Gabriel <matiascoria427@gmail.com>
2023-08-02 09:17:07 -0300

    se creo la funcionalidad para agregar usuarios

--- AdminFront/API/Personal/Personal.js
+++ AdminFront/API/Personal/Personal.js
@@ -3,30 +3,23 @@ import config from "../../config/config.js";
 const api = config.apiUrl;
 const enpointPersonal = `${api}/api/Personal`;
 
-const crearPersonal = async (personalRequest) =>{
+const crearPersonal = async (personalRequest) => {
+  const response = await fetch(enpointPersonal, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(personalRequest),
+  });
 
-    let result;
+  const result = await response.json();
 
-    const response = await fetch(enpointPersonal, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(personalRequest),
-      });
-  
-      if (!response.ok) {
-        throw new Error();
-      }
-
-      if(response.ok){
-        result = await response.json();
-      }
-      
-      return result; 
+  return {
+    response,
+    result,
+  };
 };
 
-
 export const Personal = {
-    Post : crearPersonal
-}
+  Post: crearPersonal
+};
--- AdminFront/js/callsForUsuarios.js
+++ AdminFront/js/callsForUsuarios.js
@@ -10,44 +10,66 @@ const btnCargarEmpleado = document.getElementById("btn_carga_empleado");
 
 
 btnCargarEmpleado.addEventListener("click", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
 
-    const personalNombre = document.getElementById("nombre").value;
-    const personalApellido = document.getElementById("apellido").value;
-    const personalDni = document.getElementById("dni").value;
-    const personalPrivilegio = document.getElementById("permisos_privilegio").value;
-    const personalFechaIngreso = document.getElementById("fecha_ingreso").value;
-    const personalFechaNacimiento = document.getElementById("fecha_nacimiento").value;
-    const personalCorreo = document.getElementById("correo_electronico").value;
-    const personalTelefono = document.getElementById("num_telefono").value;
+  const personalNombre = document.getElementById("nombre").value;
+  const personalApellido = document.getElementById("apellido").value;
+  const personalDni = document.getElementById("dni").value;
+  const personalPrivilegio = document.getElementById("permisos_privilegio").value;
+  const personalFechaIngreso = document.getElementById("fecha_ingreso").value;
+  const personalFechaNacimiento = document.getElementById("fecha_nacimiento").value;
+  const personalCorreo = document.getElementById("correo_electronico").value;
+  const personalTelefono = document.getElementById("num_telefono").value;
 
 
-    const personalRequest = {
-        nombre: personalNombre,
-        apellido: personalApellido,
-        dni: personalDni,
-        fecha_nacimiento: personalFechaNacimiento,
-        fecha_ingreso: personalFechaIngreso,
-        mail: personalCorreo,
-        telefono: personalTelefono,
-        privilegio: personalPrivilegio
-    }
+  const personalRequest = {
+    nombre: personalNombre,
+    apellido: personalApellido,
+    dni: personalDni,
+    fecha_nacimiento: personalFechaNacimiento,
+    fecha_ingreso: personalFechaIngreso,
+    mail: personalCorreo,
+    telefono: personalTelefono,
+    privilegio: personalPrivilegio
+  }
 
-    Swal.fire({
-        title: 'estas a punto de carga un empleado , estas seguro?',
-        text: "revisa bien los datos por favor :)",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'confirmar',
-        cancelButtonText: "cancelar"
-      }).then((result) => {
-        if (result.isConfirmed) {
-                
-            Personal.Post(personalRequest)
-            .then( result => console.log(result))
+  Swal.fire({
+    title: 'Estás a punto de cargar un empleado, ¿estás seguro?',
+    text: 'Revisa bien los datos, por favor :)',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Confirmar',
+    cancelButtonText: 'Cancelar'
+  }).then(async (resultSwal) => { // Cambio en el nombre de la variable resultSwal
+    if (resultSwal.isConfirmed) {
+
+      Personal.Post(personalRequest)
+      .then(resultado => {
+
+        if (resultado.response.ok) {
+
+            Swal.fire({
+                icon: 'success',
+                title: 'Personal creado con exito',
+                text: `ID: ${resultado.result.id}`,
+            })
+            .then(() => { location.reload() });
+
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Ups! ocurrio un problema',
+            })
         }
-      })
-})
+    })
+
+
+
+    }
+  });
+
+
+});
